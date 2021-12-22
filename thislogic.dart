efe5086b441245e9f489fb5ee7f6c9d45057900f
Malik Abdul Ahad <96419254+malikabdulahad1@users.noreply.github.com>
2021-12-22 14:29:36 +0500

    Create thislogic.dart

---
+++ thislogic.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+class Number {
+  String? a;
+  int? b;
+
+  void student(String studentname, int marks) {
+    this.a = studentname;
+    this.b = marks;
+    print(a);
+    print(b);
+  }
+}
